9eef086ade4e776a7bda2f612ad25c918dd7878f
Nils Knittel <go56bog@mytum.de>
2024-01-26 14:51:55 +0100

    moved SummaryDialog class

--- lib/pages/activity_summary.dart
+++ lib/pages/activity_summary.dart
@@ -11,8 +11,81 @@ import '../utils/general_utils.dart';
 import 'activity_display.dart';
 import 'history.dart';
 
-class ActivitySummaryPage extends StatelessWidget {
-  const ActivitySummaryPage(
+class SummaryDialog extends StatefulWidget {
+  const SummaryDialog({super.key, required this.activityDatabase});
+
+  final ActivityDatabase activityDatabase;
+
+  @override
+  State<SummaryDialog> createState() => _SummaryDialogState();
+}
+
+class _SummaryDialogState extends State<SummaryDialog>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _scaleAnimation,
+      child: Dialog(
+        insetPadding: const EdgeInsets.all(16.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16.0),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: CustomMaterialAppBar.appBar(title: 'Summary'),
+            body: Container(
+              width: double.infinity,
+              height: double.infinity,
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+                color: ColorTheme.background,
+              ),
+              child: ActivitySummary(
+                activityDatabase: widget.activityDatabase,
+                small: true,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class SummaryPage extends StatelessWidget {
+  const SummaryPage(
       {super.key, required this.activityDatabase, required this.historyState});
 
   final HistoryState historyState;
