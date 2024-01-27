33112fee6aa78d3d1a0cc580a703f12bdff60a63
Nils Knittel <go56bog@mytum.de>
2024-01-27 15:30:24 +0100

    Added comments

--- lib/activity/activity.dart
+++ lib/activity/activity.dart
@@ -16,18 +16,26 @@ import 'activity_database.dart';
 import 'activity_state.dart';
 import 'slopes.dart';
 
+/// Enum to represent the type of location service used.
 enum LocationType {
   gps,
   network,
 }
 
+/// Class representing an activity, extending LocationHandler for location-related functionality.
 class Activity extends LocationHandler {
   final int id;
 
-  Activity(
-      {required this.id,
-      LatLng currentPosition = const LatLng(0.0, 0.0),
-      bool mapDownloaded = false}) {
+  /// Constructor for Activity.
+  ///
+  /// @param id: Activity ID.
+  /// @param currentPosition: Initial geographical coordinates.
+  /// @param mapDownloaded: Flag indicating whether the map is downloaded.
+  Activity({
+    required this.id,
+    LatLng currentPosition = const LatLng(0.0, 0.0),
+    bool mapDownloaded = false,
+  }) {
     latitude = currentPosition.latitude;
     longitude = currentPosition.longitude;
     mapData.mapDownloaded = mapDownloaded;
@@ -38,13 +46,17 @@ class Activity extends LocationHandler {
     areaName = PowderPilot.locationService.areaName;
   }
 
+  /// Initialization method for the Activity.
   void init() {
     if (kDebugMode) {
       print('Activity $id initialized');
     }
+
+    /// Start the location stream in LocationHandler
     _locationStream();
   }
 
+  /// Method to start the activity.
   void startActivity() {
     /// Check if the activity is already active
     if (state.isActive) {
@@ -78,6 +90,7 @@ class Activity extends LocationHandler {
     updateData();
   }
 
+  /// Method to stop the activity.
   void stopActivity(BuildContext context) {
     /// Method to show the summary dialog after the activity is stopped
     void showCustomDialog(
@@ -127,15 +140,19 @@ class Activity extends LocationHandler {
     slope.currentSlope = 0.0;
     updateData();
 
+    /// Start the location stream in passive mode
     PowderPilot.locationService.startPassiveLocationStream();
 
+    /// Remove the location callback
     PowderPilot.locationService.removeListener(locationCallback);
 
+    /// Create a new activity
     PowderPilot.createNewActivity(
         currentPosition: LatLng(latitude, longitude),
         mapDownloaded: mapData.mapDownloaded);
   }
 
+  /// Method to add the activity to the list.
   void _addActivityToList() async {
     /// Get the number of total activities
     int numActivities = await SharedPref.readInt(PowderPilot.numActivitiesKey);
@@ -144,6 +161,7 @@ class Activity extends LocationHandler {
     SharedPref.saveInt(PowderPilot.numActivitiesKey, numActivities + 1);
   }
 
+  /// Method to pause the activity.
   void pauseActivity() {
     /// Check if the activity is already paused
     if (!state.isActive) {
@@ -166,6 +184,7 @@ class Activity extends LocationHandler {
     PowderPilot.locationService.startPassiveLocationStream();
   }
 
+  /// Method to resume the activity.
   void resumeActivity() {
     /// Check if the activity is already active
     if (!state.isActive) {
@@ -197,6 +216,7 @@ class Activity extends LocationHandler {
   }
 }
 
+/// Class to handle location-related updates and calculations for an activity.
 class LocationHandler extends ActivityData {
   int _numberOfDistanceUpdates = 0;
   int _numberOfSpeedUpdates = 0;
@@ -204,25 +224,32 @@ class LocationHandler extends ActivityData {
   int _numberOfAltitudeUpdates = 0;
   int _numberOfLocationUpdates = 0;
 
-  /// Location callback
+  /// Callback to be invoked when a location update is received.
   late LocationCallback locationCallback;
-  
-  /// Altitude buffer
-  /// Used to smooth the altitude
+
+  /// Altitude buffer used to smooth altitude data.
   final List<double> _altitudeBuffer = [];
 
-  /// Flag to track if the location is initialized
+  /// Flag to track if the location is initialized.
   bool _locationInitialized = false;
 
+  /// Method to start handling location updates.
   void _locationStream() {
+    /// Temporary location variable to store the last location.
     late Position lastLocation;
 
+    /// Temporary variable to store the current run length.
     double currentRunLength = 0.0;
 
+    /// Update speed based on the received location data.
+    ///
+    /// @param position: The current position.
     void updateSpeed(Position position) {
+      /// Filter out unrealistic speed values.
       if (position.speed < 55) {
         speed.currentSpeed = position.speed;
 
+        /// Update fastest location and route if the current speed is a new maximum.
         if (speed.currentSpeed > speed.maxSpeed) {
           activityLocations =
               activityLocations.setFastestLocation([longitude, latitude]);
@@ -230,6 +257,7 @@ class LocationHandler extends ActivityData {
           speed.maxSpeed = speed.currentSpeed;
         }
 
+        /// Handle speed updates and maintain a record of speeds.
         if (speed.currentSpeed < 0.6) {
           speed.currentSpeed = 0.0;
         } else {
@@ -241,37 +269,34 @@ class LocationHandler extends ActivityData {
 
         speed.speeds.add([
           activityTimer.duration.total.inSeconds.toDouble(),
-          double.parse(speed.currentSpeed.toStringAsFixed(1))
+          double.parse(speed.currentSpeed.toStringAsFixed(1)),
         ]);
       }
     }
 
+    /// Update altitude based on the received location data.
+    ///
+    /// @param position: The current position.
     void updateAltitude(Position position) {
       _numberOfAltitudeUpdates++;
 
-      /// Generated with GPT 3.5
-      /// Helper method to smooth the altitude
+      /// Helper method to smooth altitude using a moving average technique.
+      ///
+      /// @param newAltitude: The new altitude.
       double smoothAltitude(double newAltitude) {
-        /// Implement a simple smoothing technique (e.g., moving average)
-        /// You can customize the smoothing algorithm based on your needs.
         const int smoothingFactor = 5;
 
-        /// Adjust this value based on your preference
         _altitudeBuffer.add(newAltitude);
 
-        /// Remove the first element if the buffer is full
         if (_altitudeBuffer.length > smoothingFactor) {
           _altitudeBuffer.removeAt(0);
         }
 
-        /// Calculate the average of the buffer
         double smoothedValue =
             _altitudeBuffer.reduce((a, b) => a + b) / _altitudeBuffer.length;
 
-        /// Return the smoothed value
         return smoothedValue;
       }
-      
 
       altitude.currentAltitude = smoothAltitude(position.altitude);
       altitude.maxAltitude = altitude.currentAltitude > altitude.maxAltitude
@@ -288,13 +313,16 @@ class LocationHandler extends ActivityData {
 
       altitude.altitudes.add([
         activityTimer.duration.total.inSeconds,
-        altitude.currentAltitude.round()
+        altitude.currentAltitude.round(),
       ]);
     }
 
-    /// Temporary distance variable
+    /// Temporary distance variable.
     double tempDistance = 0.0;
 
+    /// Update distance based on the received location data.
+    ///
+    /// @param position: The current position.
     void updateDistance(Position position) {
       double calculatedDistance = Utils.calculateHaversineDistance(
           LatLng(lastLocation.latitude, lastLocation.longitude),
@@ -303,6 +331,7 @@ class LocationHandler extends ActivityData {
       _numberOfDistanceUpdates++;
 
       if (_numberOfDistanceUpdates % 5 == 0 && calculatedDistance > 2) {
+        // Reset calculated distance if it's unrealistically high.
         if (calculatedDistance > 400) {
           calculatedDistance = 0.0;
         }
@@ -350,20 +379,42 @@ class LocationHandler extends ActivityData {
       }
     }
 
-    /// Temp altitude variable for slope calculation
+    /// Temporary altitude variable for slope calculation.
     double tempAltitude = 0.0;
 
-    /// Temp location variable for slope calculation
+    /// Temporary location variable for slope calculation.
     late Position tempLocation;
+
+    /// Update slope based on the received location data.
+    ///
+    /// @param position: The current position.
     void updateSlope(Position position) {
+      /// Buffer to prevent slope from updating too frequently and to
+      /// ensure that the calculated slope is realistic.
+      double slopeBuffer = 10;
+
+      /// Buffer to prevent unrealistic differences in altitude from
+      double unrealisticallyHighDifference = 125;
+
+      /// Handle slope updates only if the activity is in
+      /// downhill or uphill mode.
       if (state.runningStatus != RunningStatus.pause) {
+        /// Calculate the difference between the current altitude and the
+        /// previous saved altitude.
         double difference = (tempAltitude - altitude.currentAltitude).abs();
-        if (difference > 125) {
+
+        /// If the difference is greater than $unrealisticallyHighDifference,
+        /// set the temporary altitude and location to the current altitude
+        /// and location because its too large to be realistic.
+        if (difference > unrealisticallyHighDifference) {
           tempAltitude = altitude.currentAltitude;
           tempLocation = position;
           difference = 0.0;
         }
-        if (difference > 10) {
+
+        /// If the difference is greater than $slopeBuffer, calculate the slope.
+        /// Formula: slope = (vertical distance / horizontal distance) * 100
+        if (difference > slopeBuffer) {
           if (tempLocation.latitude == position.latitude &&
               tempLocation.longitude == position.longitude) {
             return;
@@ -372,9 +423,7 @@ class LocationHandler extends ActivityData {
             double horizontalDistance = Utils.calculateHaversineDistance(
                 LatLng(tempLocation.latitude, tempLocation.longitude),
                 LatLng(position.latitude, position.longitude));
-
             double verticalDistance = tempAltitude - altitude.currentAltitude;
-
             slope.currentSlope = verticalDistance / horizontalDistance * 100;
             _numberOfSlopeUpdates++;
             slope.maxSlope = slope.currentSlope > slope.maxSlope
@@ -385,12 +434,19 @@ class LocationHandler extends ActivityData {
           } else {
             slope.currentSlope = 0.0;
           }
+
+          /// Update the temporary altitude and location to the current
+          /// altitude and location.
           tempAltitude = altitude.currentAltitude;
           tempLocation = position;
         }
       }
     }
 
+    /// Method to initialize location data when starting the activity.
+    /// This method is only invoked once per activity.
+    ///
+    /// @param position: The current position.
     void initializeLocation(Position position) {
       lastLocation = position;
       altitude.currentAltitude = position.altitude;
@@ -406,10 +462,16 @@ class LocationHandler extends ActivityData {
       }
     }
 
+    /// Method to be invoked when a location update is received.
+    /// This method is invoked every time a location update is received.
+    ///
+    /// @param position: The current position.
     void onLocationUpdate(Position position) {
+      /// Update location data
       latitude = position.latitude;
       longitude = position.longitude;
 
+      /// Handle location updates only if the activity is active.
       if (state.isRunning) {
         if (position.accuracy < 10) {
           gpsAccuracy = GpsAccuracy.high;
@@ -426,18 +488,19 @@ class LocationHandler extends ActivityData {
             route.addCoordinates([longitude, latitude]);
           }
 
-          // Update speed
+          /// Update speed
           updateSpeed(position);
 
-          // Update altitude
+          /// Update altitude
           updateAltitude(position);
 
-          // Update distance
+          /// Update distance
           updateDistance(position);
 
-          // Update slope
+          /// Update slope
           updateSlope(position);
 
+          /// Update nearest slope every 5 location updates.
           if (_numberOfLocationUpdates % 5 == 0) {
             _updateNearestSlope();
           }
@@ -446,36 +509,39 @@ class LocationHandler extends ActivityData {
             _locationInitialized = true;
           }
         } else {
-          // Set values to 0 if GPS accuracy is low
+          /// Set values to 0 if GPS accuracy is low
           speed.currentSpeed = 0.0;
           slope.currentSlope = 0.0;
         }
       }
-      if (PowderPilot.connectionStatus == true && !mapData.mapDownloaded) {
-        _downloadMap();
-      }
-      updateData();
 
-      if (_numberOfLocationUpdates % 15 == 0 &&
-          (PowderPilot.connectionStatus == true ||
-              _numberOfLocationUpdates == 0)) {
-        if (!mapData.mapDownloaded) {
+      /// Download the slope map if not already downloaded.
+      if (!mapData.mapDownloaded) {
+        if (_numberOfLocationUpdates % 15 == 0 &&
+            (PowderPilot.connectionStatus == true ||
+                _numberOfLocationUpdates == 0)) {
           _downloadMap();
         }
       }
-      String areaNameTemp = PowderPilot.locationService.areaName;
-      if (areaNameTemp != areaName) {
-        areaName = areaNameTemp;
-        updateData();
-      }
 
+      /// Update area name if it has changed.
+      areaName = PowderPilot.locationService.areaName;
+
+      /// Update UI
+      updateData();
+
+      /// Increment the number of location updates each time a location update is received.
       _numberOfLocationUpdates++;
     }
 
+    /// Set the location callback.
     locationCallback = onLocationUpdate;
+
+    /// Add the location callback to the location service.
     PowderPilot.locationService.addListener(locationCallback);
   }
 
+  /// Method to update the nearest slope based on the current location.
   Future<void> _updateNearestSlope() async {
     if (mapData.mapDownloaded == true) {
       if (state.isDownhill || state.isUphill) {
@@ -487,6 +553,7 @@ class LocationHandler extends ActivityData {
 
   static bool _isDownloadRunning = false;
 
+  /// Method to download map data if not already downloaded.
   Future<void> _downloadMap() async {
     if (_isDownloadRunning) {
       return;
