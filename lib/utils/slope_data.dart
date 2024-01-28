c9da5c9265f81474c5b9944a83472ed606b2c9b4
Nils Knittel <go56bog@mytum.de>
2024-01-28 22:57:15 +0100

    parallel processing of data

--- lib/utils/slope_data.dart
+++ lib/utils/slope_data.dart
@@ -89,7 +89,7 @@ class SlopeFetcher {
 
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = json.decode(response.body);
-        return await parseData(data, lift: true);
+        return await _parseData(data, lift: true);
       }
     }
     return false;
@@ -123,29 +123,46 @@ class SlopeFetcher {
 
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = json.decode(response.body);
-        return await parseData(data);
+        return await _parseData(data);
       }
     }
     return false;
   }
 
   /// Parses the fetched data and adds slope objects to the SlopeMap.
+  /// The data is processed in batches to speed up the parsing process.
   ///
   /// @param data The map containing the fetched data.
   /// @param lift A flag indicating whether the fetched data is for a lift.
   /// @return A Future<bool> indicating the success of the parse operation.
-  static Future<bool> parseData(Map<String, dynamic> data,
-      {bool lift = false}) async {
+  static Future<bool> _parseData(Map<String, dynamic> data, {bool lift = false}) async {
+    /// Create a list to store futures of the parsing logic
+    List<Future<void>> parsingFutures = [];
+
     for (final element in data['elements']) {
-      try {
-        Slope slope = Slope(slope: element, lift: lift);
-        SlopeMap.addSlope(slope);
-      } catch (e) {
-        if (kDebugMode) {
-          print(e);
-        }
+      /// Add each parsing operation to the list of futures
+      parsingFutures.add(_parseElement(element, lift));
+    }
+
+    /// Wait for all parsing operations to complete
+    await Future.wait(parsingFutures);
+
+    return true;
+  }
+
+  /// Parses a single element and adds a slope object to the SlopeMap.
+  ///
+  /// @param element The element to parse.
+  static Future<void> _parseElement(dynamic element, bool lift) async {
+    try {
+      /// Create a new slope object from the element
+      Slope slope = Slope(slope: element, lift: lift);
+      /// Add the slope object to the SlopeMap
+      SlopeMap.addSlope(slope);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
       }
     }
-    return true;
   }
 }
