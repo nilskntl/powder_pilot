7c7f3f449cf5143a2e55192d241c83f311af1959
Nils Knittel <go56bog@mytum.de>
2024-02-03 22:19:31 +0100

    new category

---
+++ lib/ui/activity/info/widgets/run.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:powder_pilot/activity/data_provider.dart';
+import 'package:powder_pilot/main.dart';
+import 'package:powder_pilot/ui/widgets/single_graph.dart';
+
+import '../../../../activity/state.dart';
+import '../../../../string_pool.dart';
+import '../../../../theme.dart';
+import '../../../../utils/general_utils.dart';
+import 'category.dart';
+
+class Run extends StatefulWidget {
+  const Run({super.key, required this.dataProvider, this.summary = false});
+
+  final ActivityDataProvider dataProvider;
+  final bool summary;
+
+  @override
+  State<Run> createState() => _RunState();
+}
+
+class _RunState extends State<Run> {
+  @override
+  Widget build(BuildContext context) {
+    return WidgetTheme.container(
+      padding: Category.paddingInside,
+      alignment: Alignment.topLeft,
+      child: ListView(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        padding: const EdgeInsets.all(0),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment:
+                PowderPilot.dataProvider.status == ActivityStatus.inactive &&
+                        !widget.summary
+                    ? CrossAxisAlignment.center
+                    : CrossAxisAlignment.start,
+            children: [
+              Category.buildHeader(title: StringPool.RUNS),
+              const SizedBox(width: 12),
+              Category.buildIcon(
+                  icon: LogoTheme.runs, targetHeight: Category.iconHeight * 2),
+            ],
+          ),
+          if (PowderPilot.dataProvider.status != ActivityStatus.inactive ||
+              widget.summary)
+            const SizedBox(height: 8.0),
+          if (PowderPilot.dataProvider.status != ActivityStatus.inactive ||
+              widget.summary)
+            Row(
+              children: [
+                Utils.buildText(
+                    text: widget.dataProvider.runs.totalRuns.toString(),
+                    fontSize: FontTheme.sizeHeader * 2,
+                    fontWeight: FontWeight.bold),
+                const Spacer(),
+                Category.buildSecondaryValueColumn(
+                  value: widget.dataProvider.runs.longestRun.toStringAsFixed(1),
+                  title: StringPool.LONGEST,
+                  unit: Measurement.unitDistance,
+                ),
+              ],
+            ),
+          if (PowderPilot.dataProvider.status != ActivityStatus.inactive ||
+              widget.summary)
+            const SizedBox(height: 8.0),
+          if (PowderPilot.dataProvider.status != ActivityStatus.inactive ||
+              widget.summary)
+            SingleGraph(
+              data: widget.dataProvider.distance.distances,
+              factor: 0.001 * Measurement.distanceFactor,
+              unit: Measurement.unitDistance,
+              color: ColorTheme.primary,
+              dummy: true,
+            ),
+        ],
+      ),
+    );
+  }
+}
