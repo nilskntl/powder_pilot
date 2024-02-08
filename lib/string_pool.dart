f2e6aa0db0ef73865adadff262abbf37c11ffde5
Nils Knittel <go56bog@mytum.de>
2024-02-08 14:55:55 +0100

    ignore non_constant_identifier_names

--- lib/string_pool.dart
+++ lib/string_pool.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:intl/intl.dart';
 
 import 'main.dart';
@@ -227,6 +229,11 @@ class StringPool {
   static String get LEGAL_TEXT_3 => Intl.message('.',
       name: 'StringPool_LEGAL_TEXT_3', locale: PowderPilot.language[0]);
 
-  static List<String> get LEGAL_TEXT =>
-      [LEGAL_TEXT_1, TERMS_OF_SERVICE, LEGAL_TEXT_2, PRIVACY_POLICY, LEGAL_TEXT_3];
+  static List<String> get LEGAL_TEXT => [
+        LEGAL_TEXT_1,
+        TERMS_OF_SERVICE,
+        LEGAL_TEXT_2,
+        PRIVACY_POLICY,
+        LEGAL_TEXT_3
+      ];
 }
