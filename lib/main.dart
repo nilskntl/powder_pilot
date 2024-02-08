b17681d2411b29027502e67738181c50c5c8fd86
Nils Knittel <go56bog@mytum.de>
2024-02-08 14:59:42 +0100

    ignore deprecated_member_use

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,7 @@ void _init() async {
       return false;
     }
 
+    // ignore: deprecated_member_use
     String locale = ui.window.locale.toString();
     if (locale.contains('_')) {
       locale = locale.split('_').first;
